35aafdf30d2c953ab59b2c698121870508c628d8
FernandoJSM <53662620+FernandoJSM@users.noreply.github.com>
2021-03-13 18:31:58 -0300

    feat(QA): Updated tests

--- QualityAnalysis/bot_test.py
+++ QualityAnalysis/bot_test.py
@@ -3,6 +3,8 @@
 """
 from configparser import ConfigParser
 
+from TelegramBot.telegram_bot import TelegramBot
+
 if __name__ == '__main__':
 
     config_path = '../data/settings.cfg'
@@ -10,5 +12,9 @@ if __name__ == '__main__':
     cfg_parser.read(config_path)
 
     bot_token = cfg_parser.get('bot_setup', 'BOT_TOKEN')
+    messages_path = '../data/stored_messages.json'
 
-    a = 1
+    telegram_bot = TelegramBot(token=bot_token, message_data=messages_path)
+    telegram_bot.start_pooling()
+
+    a = 0
---
+++ QualityAnalysis/web_scrapper_test.py
@@ -0,0 +1,19 @@
+"""
+    Initializes webscrapper for debug purposes
+"""
+from configparser import ConfigParser
+
+from TelegramBot.web_scrapper import WebScrapper
+
+if __name__ == '__main__':
+
+    config_path = '../data/settings.cfg'
+    cfg_parser = ConfigParser()
+    cfg_parser.read(config_path)
+
+    url = cfg_parser.get('web_scrapper_setup', 'URL')
+    messages_path = '../data/stored_messages.json'
+
+    scrapper = WebScrapper(url=url, message_data=messages_path)
+
+    a = 0
