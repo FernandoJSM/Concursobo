91ef7618d1d751b8fcbec29e87d950cfac4a4ad6
FernandoJSM <53662620+FernandoJSM@users.noreply.github.com>
2021-03-14 14:50:48 -0300

    feat(readme): Updated readme

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
     license='GPL 2.0',
     author='Fernando Magalhaes',
     author_email='fernandomagalhaes@yahoo.com.br',
-    description='MarinhoBot, um bot de telegram para facilitar o acesso as informações do concurso CP-CEM da Marinha '
+    description='Marinhobô, um bot de telegram para facilitar o acesso as informações do concurso CP-CEM da Marinha '
                 'do Brasil'
 )
