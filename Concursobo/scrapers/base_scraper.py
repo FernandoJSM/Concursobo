593d0ab882707396ef52b411b2fc33309013b45f
FernandoJSM <53662620+FernandoJSM@users.noreply.github.com>
2021-11-27 11:26:33 -0300

    Adição da classe BaseScraper

---
+++ Concursobo/scrapers/base_scraper.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+
+
+class BaseScraper(ABC):
+    """
+        Classe de base para os scrapers implementados no código
+    """
+
+    @abstractmethod
+    def scrape_page(self):
+        """
+            Coleta os dados da página do concurso
+        """
+        pass
+
+    @abstractmethod
+    def updated_data(self):
+        """
+            Retorna os dados que foram atualizados
+        """
+        pass
+
+    @abstractmethod
+    def full_data(self):
+        """
+           Retorna todos os dados salvos da página
+        """
+        pass
+
+    @abstractmethod
+    def short_data(self):
+        """
+            Retorna os dados da página de forma resumida
+        """
+        pass
